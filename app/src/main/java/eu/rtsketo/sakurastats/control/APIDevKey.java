33d983be117ff6d81099d8f9a6cf41d5334428d4
rtsketo <rtsketo@gmail.com>
2019-09-19 05:05:45 +0300

    chmod=+x gradlew

--- app/src/main/java/eu/rtsketo/sakurastats/control/APIDevKey.java
+++ app/src/main/java/eu/rtsketo/sakurastats/control/APIDevKey.java
@@ -4,6 +4,6 @@ public class APIDevKey {
     /* Optionally a developer key from RoyaleAPI (link below)
      https://docs.royaleapi.com/#/authentication?id=generating-new-keys
      and a GitHub access token for users to be able to anonymously report an issue */
-    public static final String devKey = "Enter your RoyaleAPI developer key here";
-    public static final String gitKey = "Enter your GitHub access token here";
+    public static final String devKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6OTg2LCJpZGVuIjoiMTc3NDg1Nzg0OTIyODQ5MjgxIiwibWQiOnsidXNlcm5hbWUiOiJEZWFkbHlBbGl2ZSIsImtleVZlcnNpb24iOjMsImRpc2NyaW1pbmF0b3IiOiI3MjI5In0sInRzIjoxNTU2ODAzNTY4ODMzfQ.737w_nIvIfDWozWxJWhRphKTF9WTae-0yppcE_FhG9c";
+    public static final String gitKey = "38d632ac7e0dfd806fa6f375a45f35f6a986ca54";
 }
